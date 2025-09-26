ceffcbd946f2da36905d5a62d8fdf13950bbf942
Dinh Hoang Phi <phibeo345@gmail.com>
2025-09-26 14:53:11 +0700

    update: sửa lỗi nối theme
    Sửa thành công lỗi nối theme; Branch và commit này chưa bao gồm các chức năng mà nhóm đã làm ở nhánh main, giờ pull nhánh này về tích hợp xong các chức năng cũ rồi hãy mới pull req về main.

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,33 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import HomePage from "./pages/home/HomePage";
-import LoginPage from "./pages/login/LoginPage";
-import OidcCallback from "./pages/auth/OidcCallback";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
-import AccountPage from "./pages/account/AccountPage.tsx";
-import ChangePasswordPage from "./pages/account/ChangePasswordPage.tsx";
+import React from 'react';
+
+import './assets/scss/themes.scss';
+
+import Route from './Routes';
+//
+// import fakeBackend from "./helpers/AuthType/fakeBackend";
+//
+// fakeBackend();
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route
-          index
-          element={
-            <ProtectedRoute>
-              <HomePage />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="auth/callback" element={<OidcCallback />} />
-        <Route
-          path="account"
-          element={
-            <ProtectedRoute>
-              <AccountPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="account/change-password"
-          element={
-            <ProtectedRoute>
-              <ChangePasswordPage />
-            </ProtectedRoute>
-          }
-        />
-      </Route>
-    </Routes>
+    <React.Fragment>
+      <Route />
+    </React.Fragment>
   );
 }
 
 export default App;
+// import React from "react";
+// import Hello from "pages/Pages/Blogs/GridView/index";
+//
+// function App() {
+//     return (
+//         <div>
+//             <h1>Test Component</h1>
+//             <Hello />
+//         </div>
+//     );
+// }
+//
+// export default App;
+
