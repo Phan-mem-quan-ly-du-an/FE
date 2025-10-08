185848c4f5968ab1abaa09ac51e1a3c7c97981db
Dinh Hoang Phi <phibeo345@gmail.com>
2025-10-08 11:11:17 +0700

    Goi api

--- src/apiCaller/companies.tsx
+++ src/apiCaller/companies.tsx
@@ -1,8 +1,9 @@
 import ApiCaller from "./caller/apiCaller";
 
-export const callApiGetAllCompanies = async () => {
+export const callApiGetAllCompanies = async ({ pageParam }: any) => {
+    console.log(pageParam)
   const {data} = await new ApiCaller()
       .setUrl('/companies/admin/all')
       .get()
-    console.log(data)
+return data
 }
